1235cd38ca6e871a23710d3a17b288be5a41fc5a
Sshariaz <166885831+Sshariaz@users.noreply.github.com>
2024-10-09 05:42:21 +0000

    commit 1

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+/*sharia zainab*/
+import React from 'react';
 import './App.css';
+import ToDoList from './ToDoList'; // Import the new component
+import { Container, Row, Col } from 'react-bootstrap'; // Import Bootstrap components
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Assignment 2: Omer's ToDo List</h1>
       </header>
+      <Container>
+        <Row>
+          <Col sm={8}> {/* ToDo list on the right */}
+            <ToDoList />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
